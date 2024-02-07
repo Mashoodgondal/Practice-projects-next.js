0227c01a185a9b0adccf2379f5a54dc79bb9499b
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-02-07 23:18:43 +0500

    commit

--- shoping-page/app/(components)/navebar/page.js
+++ shoping-page/app/(components)/navebar/page.js
@@ -10,7 +10,7 @@ const Nave = () => {
   };
   return (
     <div>
-      <nav className=" sticky bg-gray-400 shadow-lg shadow-gray-600 dark:bg-gray-800">
+      <nav className=" sticky bg-gray-300 shadow-lg shadow-gray-400 dark:bg-gray-100">
         <div className="max-w-screen-xl flex justify-between px-4 py-3 mx-auto">
           <div>
             <a href="#" className="text-indigo-600 text-3xl font-bold">
@@ -77,70 +77,78 @@ const Nave = () => {
         </div>
       </nav>
       {/* MOBILE Nave */}
-      <div className="md:hidden p-6 bg-whitw w-full">
-        <div className=" p-8 border-b border-green-300   flex item-center justify-between">
-          <div>
-            <a href="#" className="text-indigo-600  text-3xl font-bold">
-              Logo
-            </a>
+      <div className={`${nav ? "md:hidden p-6 bg-whitw w-full" : ""}`}>
+        <div
+          className={`${
+            nav
+              ? " w-[80%] sm:w-[70%] md:w-[85%]  p-10 ease-in duration-500"
+              : "fixed left-[-100%] p-10 ease-in duration-1000"
+          }`}
+        >
+          <div className=" p-8 border-b border-green-300   flex item-center justify-between">
+            <div>
+              <a href="#" className="text-indigo-600  text-3xl font-bold">
+                Logo
+              </a>
+            </div>
+            <div
+              onClick={handleNave}
+              className=" rounded-full shadow-lg shadow-gray-500"
+            >
+              <RiCloseCircleFill size={35} />
+            </div>
           </div>
-          <div
-            onClick={handleNave}
-            className=" rounded-full shadow-lg shadow-gray-500"
-          >
-            <RiCloseCircleFill size={35} />
-          </div>
+          <ul className="flex flex-col uppercase space-y-2 mt-6 font-bold text-gray-600 ml-8 ">
+            <li>
+              <a
+                href="#"
+                className="text-gray-900 hover:text-blue-600 dark:text-white "
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-gray-900 hover:text-blue-600 dark:text-white "
+              >
+                Company
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-gray-900 hover:text-blue-600 dark:text-white  "
+              >
+                Team
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-gray-900 hover:text-blue-600 dark:text-white "
+              >
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-gray-900 hover:text-blue-600 dark:text-white "
+              >
+                Blogs
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="text-gray-900 hover:text-blue-600 dark:text-white "
+              >
+                HOT OFFERS
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul className="flex flex-col uppercase space-y-2 mt-6 font-bold text-gray-600 ml-8 ">
-          <li>
-            <a
-              href="#"
-              className="text-gray-900 hover:text-blue-600 dark:text-white "
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="text-gray-900 hover:text-blue-600 dark:text-white "
-            >
-              Company
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="text-gray-900 hover:text-blue-600 dark:text-white  "
-            >
-              Team
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="text-gray-900 hover:text-blue-600 dark:text-white "
-            >
-              Features
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="text-gray-900 hover:text-blue-600 dark:text-white "
-            >
-              Blogs
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="text-gray-900 hover:text-blue-600 dark:text-white "
-            >
-              HOT OFFERS
-            </a>
-          </li>
-        </ul>
       </div>
     </div>
   );
