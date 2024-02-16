b4cb9077f752860ad89a2ed3c7e78b6124bd2437
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-02-16 23:08:07 +0500

    commit

---
+++ shoping-page/app/heroSection/page.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+import img1 from "@/app/image/coat2.jpg";
+import img2 from "@/app/image/coat1.jpg";
+import img3 from "@/app/image/comp2.jpg";
+import img4 from "@/app/image/comp1.jpg";
+import img5 from "@/app/image/lg1.jpg";
+import img6 from "@/app/image/lg2.jpg";
+import Card from "../(components)/card/page";
+
+const Hero = () => {
+  return (
+    <div className="p-6 md:p-4 grid  md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-6 md:text-center  ">
+      <div>
+        <Card
+          img={img5}
+          title="Boost your business"
+          para=" Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order"
+        />
+      </div>
+      <div>
+        <Card
+          img={img4}
+          title="Labtop: Desire genaration "
+          para=" Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order"
+        />
+      </div>
+      <div>
+        <Card
+          img={img3}
+          title="PC for progress"
+          para=" Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order"
+        />
+      </div>
+      <div>
+        <Card
+          img={img2}
+          title="Ladies Shopping"
+          para=" Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order"
+        />
+      </div>
+      <div>
+        <Card
+          img={img1}
+          title="Find your dress"
+          para=" Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order"
+        />
+      </div>
+      <div>
+        <Card
+          img={img6}
+          title="Bag detaile shopping"
+          para="  Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- shoping-page/app/page.js
+++ shoping-page/app/page.js
@@ -1,9 +1,21 @@
-import Slide from "./slide/page";
+import Hero from "./heroSection/page";
+import Carousel from "./slide/page";
+// import Slide from "./slide/page";
+const images = [
+  "https://via.placeholder.com/600x400?text=Image+1",
+  "https://via.placeholder.com/600x400?text=Image+2",
+  "https://via.placeholder.com/600x400?text=Image+3",
+];
 
 export default function Home() {
   return (
     <>
-      <Slide />
+      <div className="container mx-auto text-center py-12">
+        <h1 className="text-4xl font-bold mb-8">Simple Carousel Example</h1>
+        {images.length > 0 && <Carousel images={images} />}
+      </div>
+      {/* <Carousel /> */}
+      <Hero />
     </>
   );
 }
