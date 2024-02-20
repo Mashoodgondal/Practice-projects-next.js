6705391a69a560100f8c1d58bfede150706a3c49
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-02-20 19:05:10 +0500

    commit

---
+++ shoping-page/app/frontPage/page.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import img from "@/app/image/mall.jpg";
+
+const Front = () => {
+  return (
+    <div>
+      <div className="relative m-8">
+        <Image
+          src={img}
+          alt="shopping image"
+          className="w-[1100px] h-[400px] mt-8 mx-auto  "
+        />
+        <div className="absolute bottom-9 left-1/2 transform -translate-x-1/2 text-red-600 cursor-pointer hover:font-bold hover:text-red-700 text-3xl">
+          "Discover your style, one click at a time."
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Front;
--- shoping-page/app/page.js
+++ shoping-page/app/page.js
@@ -1,27 +1,10 @@
+import Front from "./frontPage/page";
 import Hero from "./heroSection/page";
-import Carousel from "./slide/page";
-import img5 from "./image/comp1.jpg";
-const images = [
-  { src: { img5 }, alt: "Image 1", text: "Text Overlay 1" },
-  { src: "/image2.jpg", alt: "Image 2", text: "Text Overlay 2" },
-  { src: "/image3.jpg", alt: "Image 3", text: "Text Overlay 3" },
-  // Add more images as needed
-];
 
 export default function Home() {
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {images.map((image, index) => (
-          <Carousel
-            key={index}
-            src={image.src}
-            alt={image.alt}
-            text={image.text}
-          />
-        ))}
-      </div>
-      {/* <Carousel /> */}
+      <Front />
       <Hero />
     </>
   );
