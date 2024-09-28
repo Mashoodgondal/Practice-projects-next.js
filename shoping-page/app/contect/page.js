b47d98b7e2025d9710821400bae346708842a498
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-09-28 16:18:27 +0500

    commit

--- shoping-page/app/contect/page.js
+++ shoping-page/app/contect/page.js
@@ -1,90 +1,48 @@
+"use client";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "@/app/config/firebase";
+import { useState } from "react";
+
 const Contect = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [course, setCourse] = useState("");
+  const submitHandeler = async () => {
+    let student = {
+      name: userName,
+      email,
+      course,
+    };
+    try {
+      const collectionName = collection(db, "abs");
+      await addDoc(collectionName, student);
+      console.log("Document written with ID:");
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
     <div>
       <h1 className="font-bold text-indigo-600 text-5xl text-center">
         Contect us directly
       </h1>
 
-      <form className="p-4 mt-10 mx-auto text-center space-y-4">
-        <div>
-          <label>
-            Name
-            <input placeholder="Enter your name"></input>
-          </label>
-        </div>
-        <div>
-          <label>
-            Email
-            <input placeholder="Enter your email"></input>
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input placeholder="Enter your password"></input>
-          </label>
-        </div>
-      </form>
-      <form className="p-4 mt-10 mx-auto text-center space-y-4">
-        <div>
-          <label>
-            Name
-            <input placeholder="Enter your name"></input>
-          </label>
-        </div>
-        <div>
-          <label>
-            Email
-            <input placeholder="Enter your email"></input>
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input placeholder="Enter your password"></input>
-          </label>
-        </div>
-      </form>
-      <form className="p-4 mt-10 mx-auto text-center space-y-4">
-        <div>
-          <label>
-            Name
-            <input placeholder="Enter your name"></input>
-          </label>
-        </div>
-        <div>
-          <label>
-            Email
-            <input placeholder="Enter your email"></input>
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input placeholder="Enter your password"></input>
-          </label>
-        </div>
-      </form>
-      <form className="p-4 mt-10 mx-auto text-center space-y-4">
-        <div>
-          <label>
-            Name
-            <input placeholder="Enter your name"></input>
-          </label>
-        </div>
-        <div>
-          <label>
-            Email
-            <input placeholder="Enter your email"></input>
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input placeholder="Enter your password"></input>
-          </label>
-        </div>
-      </form>
+      {/* <form className="p-4 mt-10 mx-auto text-center space-y-4"> */}
+      <input
+        placeholder="Enter your name"
+        onChange={(e) => setUserName(e.target.value)}
+      ></input>
+      <input
+        placeholder="Enter your email"
+        onChange={(e) => setEmail(e.target.value)}
+      ></input>
+      <input
+        placeholder="Enter cource name"
+        onChange={(e) => setCourse(e.target.value)}
+      ></input>
+      <button onClick={submitHandeler}>submit</button>
+      {/* </form> */}
     </div>
   );
 };
